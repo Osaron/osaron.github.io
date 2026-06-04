a1cef3a90661948899bd53dd6a4f707538076acf
RONDON Oscar Andres <T0309801@gemalto.com>
2026-06-04 11:02:28 -0500

    feat: make user guide rows clickable to open PDF, add hover effect

--- angular-app/src/app/pages/projects/user-guides/user-guides.ts
+++ angular-app/src/app/pages/projects/user-guides/user-guides.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { GuideService } from '../../../core/services/guide.service';
+import { Guide } from '../../../core/models/guide.model';
 
 @Component({
   selector: 'app-user-guides',
@@ -10,4 +11,8 @@ import { GuideService } from '../../../core/services/guide.service';
 })
 export class UserGuides {
   guides$ = inject(GuideService).getAll();
+
+  openPdf(guide: Guide) {
+    window.open(`/projects/user-guides/${guide.slug}/manual.pdf`, '_blank', 'noopener');
+  }
 }
